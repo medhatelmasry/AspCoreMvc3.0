c1b3ef8088aaf7d64fdd0324a8af1a50ab222aa6
Medhat Elmasry <A00127241@ad.bcit.ca>
2019-09-19 14:49:36 -0700

    1st commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspcoreMvc.Models;
+
+namespace AspcoreMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Hello()
+        {
+            return View();
+        }
+
+        
+        public IActionResult Bingo()
+        {
+            //ViewData["nice"] = "This is coming from the Bingo action method";
+            ViewBag.nice = "This is coming from the Bingo ACTION method in the Home controller";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
